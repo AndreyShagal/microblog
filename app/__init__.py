c455a31677f8eb94301326e73e21b5c8827d25d5
andrey <andrey.shagal@gmail.com>
2015-09-24 11:35:22 -0400

    init

--- app/__init__.py
+++ app/__init__.py
@@ -3,4 +3,6 @@ __author__ = 'andrey'
 from flask import Flask
 
 app = Flask(__name__)
+app.config.from_object('config')
+
 from app import views
--- app/views.py
+++ app/views.py
@@ -3,8 +3,9 @@ import sys
 reload(sys)
 sys.setdefaultencoding("utf-8")
 
-from  flask import render_template
+from  flask import render_template, flash, redirect
 from app import app
+from forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
@@ -22,9 +23,27 @@ def index():
 
         {
             'author': { 'nickname': 'Megust' },
-            'body': '111в'
+            'body': 'пинг пинг'
+        },
+
+        {
+            'author': { 'nickname': 'Шмегуст' },
+            'body': 'понг понг'
         }
 
 
     ]
     return render_template("index.html", title = 'Home', user = user, posts = posts )
+
+
+
+@app.route('/login', methods = ['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for OpenID="' + form.openid.data + '", remember_me=' + str(form.remember_me.data))
+        return redirect('/index')
+    return render_template('login.html',
+                           title = 'Sign In',
+                           form = form,
+                           providers = app.config['OPENID_PROVIDERS'])
